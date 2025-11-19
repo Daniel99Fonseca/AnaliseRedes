fa9930e369a43947e4fce48c57b6a0b8e18fc655
Daniel <dasfa1@iscte-iul.pt>
2025-11-19 20:08:40 +0000

    Código da Q1 feita

--- AR_ParteI.R
+++ AR_ParteI.R
@@ -1,20 +1,14 @@
-install.packages("igraph")
+# install.packages("igraph")
 library(igraph)
 
-#Leitura dos dados e contrução do grafo
-dados <- read.table("C:/Users/GuiPrs335PC/RstudioFiles/trab.txt")
+# Leitura dos dados e construção do grafo
+dados <- read.table("trab.txt")
 g <- graph_from_data_frame(dados, directed = FALSE)
 g
-
-"Eu sou gui"
-ola <- "eu sou o daniel"
-joao <- 13
-"É preciso fazer push?"
 
 # Parte I
 
 #a)
-
 # Dimensão/Número de nodos -> 113
 N_nod <- vcount(g)
 
@@ -70,4 +64,104 @@ table(core)
 #g) "Elabore um pequeno texto com comentários adicionais que considere pertinentes."
 
 #h) "Tendo em conta o contexto, identifique algumas características/o papel de alguns dos presentes na festa. Justifique."
+
+
+
+### Parte II
+
+############
+#### Q1 ####
+############
+
+# Modelo Preferential-Attachment
+# n(odes) = 500 nodos
+# e(dges) = 1000 ligacoes
+set.seed(5158)
+rede <- sample_pa(n = 500, power = 1, m = 2, directed = FALSE)
+
+ecount(rede)
+
+vcount(rede)
+
+plot(rede, vertex.size = 5, vertex.label = NA, edge.color = "grey")
+
+# i) determine a densidade e classifique a rede;
+# Bastante esparsa com densidade 0.008
+densidade_pa <- edge_density(rede)
+
+# ii) indique se a rede é conexa;
+# TRUE
+is_connected(rede)
+
+# iii) - obtenha o grau médio e a distribuição de grau.
+# Caracterize a distribuição de grau (mediana, quartis, etc.) e comente;
+graus <- degree(rede)
+
+avg_grau <- mean(graus)
+summary(graus)
+
+# Difícil visualização
+hist(graus, breaks = 30, col = "skyblue",
+     main = "Distribuição de Graus",
+     xlab = "Grau (número de ligações)",
+     ylab = "Número de nodos")
+
+# Calcular distribuição acumulada
+graus_sorted <- sort(graus)
+cdf <- ecdf(graus_sorted)
+
+# Plot CDF
+plot(cdf, main = "Distribuição acumulada de grau",
+     xlab = "Grau", ylab = "Fração de nodos ≤ k",
+     verticals = TRUE, do.points = FALSE)
+
+
+# iv) - calcule o parâmetro de heterogeneidade. 
+# Indique o que pode concluir quanto à existência de hubs;
+grau_medio <- mean(graus)
+grau2_medio <- mean(graus^2)
+
+H <- grau2_medio / (grau_medio^2) # H = 2.13!
+
+Q3 <- quantile(graus, 0.75)
+sum(graus > Q3)        # número de nodos acima de Q3
+sum(graus > Q3) / length(graus) * 100  # percentual da rede
+
+sum(graus > 10)
+sum(graus > 10) / length(graus) * 100
+hubs <- graus > 10
+
+
+layout <- layout_with_fr(rede, niter = 2000, area = vcount(rede)^2 * 50)
+vertex_sizes <- pmin(graus + 2, 6)  # limita tamanho máximo
+plot(rede, layout = layout,
+     vertex.size = vertex_sizes,
+     vertex.color = ifelse(graus > quantile(graus, 0.75), "red", "skyblue"),
+     vertex.label = NA,
+     edge.color = "grey",
+     margin = 0)
+
+# v) estude a associação de grau e indique o que poderá concluir-se;
+assor <- assortativity_degree(rede, directed=FALSE)
+assor # r < 0 é disassociativa, logo os hubs não estão conetados entre si.
+
+# vi) - determine a média dos comprimentos dos caminhos mais curtos e 
+# o diâmetro da rede. # Indique o que pode concluir-se quanto à distância média;
+
+avg_path <- mean_distance(rede, directed = FALSE)
+avg_path
+
+diam <- diameter(rede, directed = FALSE)
+diam
+
+# vii) - determine os coeficientes de clustering da rede. 
+# Diga o que pode concluir-se quanto à existência de triângulos.
+clust_media <- transitivity(rede, type = "average")
+clust_media
+
+
+
+
+
+
 
